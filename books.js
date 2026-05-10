dfbc59ad7309a2a830e4b39cdec69a13b259e243
Alok Kumar <118843898+alokkushwaha08@users.noreply.github.com>
2026-05-11 04:28:16 +0530

    schema Validations

--- books.js
+++ books.js
@@ -9,3 +9,34 @@ main().then((res) =>{
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
+
+//schema validations
+const bookSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    author:{
+        type:String,
+    },
+    price:{
+        type:Number,
+    },
+
+});
+
+const Book=mongoose.model("Book",bookSchema);
+
+let book1 =new Book({
+    title:"12th Rd sharma",
+    author:"xyz",
+    price:1200,
+});
+
+book1.save()
+.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+});
--- index.js
+++ index.js
@@ -14,11 +14,13 @@ async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
 
-const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    age:Number,
-})
+
+
+// const userSchema = new mongoose.Schema({
+//     name:String,
+//     email:String,
+//     age:Number,
+// })
 
 const user=mongoose.model("User",userSchema);
 
